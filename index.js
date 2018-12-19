03c29df794a726e056f31985834f804438ead04b
Peter Nguyen <peternguyen160@gmail.com>
2018-12-19 04:51:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var katzDeli = [];
 function takeANumber(line, name){
   line.push(name);
 
-  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
 
+  console.log(`Welcome ${name}. You are number ${line.length} in line.`);
   return "Welcome, " + name + ". You are number " + line.length + " in line."
 }
 
