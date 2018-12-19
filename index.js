016be93bdb9e14647a8a495721556c1946a112da
Peter Nguyen <peternguyen160@gmail.com>
2018-12-19 04:48:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ function currentLine(line){
   if(!line.length) {
     return "The line is currently empty.";
   }
-  var lineNamesandNumbers = [];
+  var lineName = [];
 
   for(var i=0; i<line.length; i++) {
-    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+    lineName.push(i+1 + ". "+ line[i]);
   }
-  console.log("The line is currently: " + lineNamesandNumbers)
-  return "The line is currently: " + lineNamesandNumbers.join(', ');
+  console.log("The line is currently: " + lineName)
+  return "The line is currently: " + lineName.join(', ');
 }
 
 
