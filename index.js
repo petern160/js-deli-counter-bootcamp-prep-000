87b2c71ee5dae4a5949832df98d285200d3cd3bb
Peter Nguyen <peternguyen160@gmail.com>
2018-12-19 04:45:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var katzDeli = [];
+function takeANumber(line, name){
+  line.push(name);
+
+  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+
+  return "Welcome, " + name + ". You are number " + line.length + " in line."
+}
 
 function currentLine(line){
   if(!line.length) {
     return "The line is currently empty.";
   }
   var lineNamesandNumbers = [];
-  
+
   for(var i=0; i<line.length; i++) {
     lineNamesandNumbers.push(i+1 + ". "+ line[i]);
   }
@@ -23,19 +30,13 @@ function nowServing(line) {
   }
 }
 
-function takeANumber(line, name){
-  line.push(name);
-  
-  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
-  
-  return "Welcome, " + name + ". You are number " + line.length + " in line."
-}
+
 takeANumber(katzDeli, "Ada")
 takeANumber(katzDeli, "Grace")
 takeANumber(katzDeli, "Kent")
 currentLine(katzDeli);
 nowServing(katzDeli);
-takeANumber(katzDeli, "Matz"); 
-currentLine(katzDeli); 
+takeANumber(katzDeli, "Matz");
+currentLine(katzDeli);
 nowServing(katzDeli);
 currentLine(katzDeli)
